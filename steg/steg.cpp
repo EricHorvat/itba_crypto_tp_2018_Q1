25ea3bd4d18647e36ccfe62d75f98f35088d7e5e
Eric Horvat <eric.nahuel.horvat@gmail.com>
2018-05-10 22:13:02 -0300

    [ADD] basic files

---
+++ steg/steg.cpp
@@ -0,0 +1,11 @@
+//
+// Created by eric on 10/05/18.
+//
+
+#include "steg.h"
+namespace steg{
+
+    void steg(std::string porter_filename, std::string info_filename, std::string destiny_filename) {
+
+    }
+};
---
+++ steg/steg.h
@@ -0,0 +1,18 @@
+//
+// Created by eric on 10/05/18.
+//
+
+#ifndef CRIPTO_TP_STEG_H
+#define CRIPTO_TP_STEG_H
+
+
+#include <string>
+
+class steg {
+
+    void steg(std::string porter_filename, std::string info_filename, std::string destiny_filename);
+
+};
+
+
+#endif //CRIPTO_TP_STEG_H
